20e2d3569c282eae5d5e1eaf09400c7d07c520c7
Kouros Owzar <owzar001@duke.edu>
2015-01-17 00:00:00 +0000

    version 0.23.2

--- R/sim.snp.expsurv.sctest.R
+++ R/sim.snp.expsurv.sctest.R
@@ -1,5 +1,5 @@
 sim.snp.expsurv.sctest <-
-function(n,gtprev,lam,a,b,test,diag=FALSE)
+function(n,gtprev,lam,a,b,ztest,diag=FALSE)
   {
     ng=as.integer(rmultinom(1,n,gtprev))
     atime=rexp(n,rep(lam,ng))
@@ -7,19 +7,17 @@ function(n,gtprev,lam,a,b,test,diag=FALSE)
     otime=pmin(atime,ctime)
     event=as.integer(atime<ctime)
     SNP=rep(0:2,ng)
-    if(test=="genotype")
-      SNP=factor(SNP)
-    else if(test=="additive")
-      SNP=SNP
-    else if(test=="dominant")
-      SNP=factor(ifelse(SNP==0,0,1))
-    else if(test=="recessive")
-      SNP=factor(ifelse(SNP==2,1,0))
-    else
-      print("test not implemented")
+    
+    SNP[SNP==0]=ztest[1]
+    SNP[SNP==1]=ztest[2]
+    SNP[SNP==2]=ztest[3]
+      
+    SNP=factor(SNP)
+    
     mod=summary(coxph(Surv(otime,event)~SNP))
     if(diag)
       print(mod)
     c(event=mean(event),pval=mod$sctest[3])
   }
-
+  
+  
--- R/surv.exp.gt.model.R
+++ R/surv.exp.gt.model.R
@@ -1,19 +1,9 @@
-surv.exp.gt.model <-
-function(pilm,lm,gtprev,GRR,model,interval)
+surv.exp.gt.model <- function(pilm,lm,gtprev,GRR,zmodel,interval)
   {
-    if(model=="dominant")
-      ww=c(1,GRR,GRR)
-    else if(model=="recessive")
-      ww=c(1,1,GRR)
-    else if (model=="additive")
-      ww=c(1,GRR,GRR^2)
-    else
-      print("model not implemented")
-    foo=function(lam0)
-      {
+    ww=GRR^zmodel
+    foo=function(lam0) {
         ww=-lam0*ww*lm
         pilm-sum(exp(ww)%*%gtprev)
-      }
+    }
     ww*uniroot(foo,interval)$root
   }
-
